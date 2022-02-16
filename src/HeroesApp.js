57bd84b3ca147760a751120ee36f7a1b25f25ce2
Yeison <enmanuelquiterios@gmail.com>
2022-02-15 22:46:50 -0500

    Added Provider

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,8 +1,18 @@
-import { AppRouter } from './routers/AppRouter';
-
-
+import { AppRouter } from "./routers/AppRouter";
+import { AuthContext } from "./auth/authContext";
+import { useReducer } from "react";
+import { authReducer } from "./auth/authReducer";
+const init = () => {
+  return {
+    logged: true,
+    name: "Yeison Casado",
+  };
+};
 export const HeroesApp = () => {
-    return (
-        <AppRouter />
-    )
-}
+  const [user, dispatch] = useReducer(authReducer, {}, init);
+  return (
+    <AuthContext.Provider value={{ user, dispatch }}>
+      <AppRouter />
+    </AuthContext.Provider>
+  );
+};
