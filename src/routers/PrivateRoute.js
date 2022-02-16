499bd670f60c9a015bdb322daab8e21906ba47e7
Yeison <enmanuelquiterios@gmail.com>
2022-02-16 12:25:38 -0500

    Fixed bug with childrens in Private Routes component

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,10 +1,12 @@
 import { useContext } from "react";
 import { AuthContext } from "../auth/authContext";
 import { Navigate } from "react-router-dom";
-const PrivateRoute = ({ childre }) => {
+
+// Import Notes here I needed to write children the entire word for my Component to render It's childrens
+const PrivateRoute = ({ children }) => {
   const { user } = useContext(AuthContext);
   console.log(user);
-  return user.logged ? childre : <Navigate to="/login" />;
+  return user.logged ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
