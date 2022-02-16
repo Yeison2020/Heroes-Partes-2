313c7bb41ceafe3c074d9e440e342d2e198dfbd2
Yeison <enmanuelquiterios@gmail.com>
2022-02-16 11:01:34 -0500

    Added user when Logged using local storage

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,28 +1,34 @@
-import { useNavigate } from 'react-router-dom';
-
-
+import { useNavigate } from "react-router-dom";
+import { types } from "../../types/types";
+import { useContext } from "react";
+import { AuthContext } from "../../auth/authContext";
 export const LoginScreen = () => {
+  console.log(types);
+  const navigate = useNavigate();
+  const { dispatch } = useContext(AuthContext);
 
-    const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate("/marvel", {
+      replace: true,
+    });
 
-    const handleLogin = () => {
-        navigate('/marvel', {
-            replace: true
-        });
-    }
+    const action = {
+      type: types.login,
+      payload: {
+        name: "Yeison Enmanuel",
+      },
+    };
+    dispatch(action);
+  };
 
+  return (
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
 
-    return (
-        <div className="container mt-5">
-            <h1>Login</h1>
-            <hr />
-
-            <button 
-                className="btn btn-primary"
-                onClick={ handleLogin }
-                >
-                    Login
-            </button>
-        </div>
-    )
-}
+      <button className="btn btn-primary" onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
+};
