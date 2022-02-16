3ddb20837f1ffcbbc71780337c5538f67cc09df3
Yeison <enmanuelquiterios@gmail.com>
2022-02-15 22:59:02 -0500

    Adding init parse to my locaStorage

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -3,10 +3,7 @@ import { AuthContext } from "./auth/authContext";
 import { useReducer } from "react";
 import { authReducer } from "./auth/authReducer";
 const init = () => {
-  return {
-    logged: true,
-    name: "Yeison Casado",
-  };
+  return JSON.parse(localStorage.getItem("user")) || { logged: false };
 };
 export const HeroesApp = () => {
   const [user, dispatch] = useReducer(authReducer, {}, init);
